9d75848a202ae8eb596f3c2e1ec3a4b442033aa5
Marina Farias <marinafarias.mk@gmail.com>
2022-01-30 14:12:46 -0300

    incluído rotas de artes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ArtesComponent } from './artes/artes.component';
 import { PsicologiaComponent } from './psicologia/psicologia.component';
 import { DireitoComponent } from './direito/direito.component';
 import { SugestaoComponent } from './sugestao/sugestao.component';
@@ -7,6 +8,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 import { SagasComponent } from './sagas/sagas.component';
+import { MelhoresdomesComponent } from './melhoresdomes/melhoresdomes.component';
 
 const enviarSemLogin = () => redirectUnauthorizedTo(['/app-app-cadastro']);
 
@@ -26,11 +28,27 @@ const routes: Routes = [
     loadChildren: () => import('./cdd/cdd.module').then(c => c.CddModule),
     ...canActivate(enviarSemLogin)
   },
+  { path:'melhores', component: MelhoresdomesComponent,
+  ...canActivate(enviarSemLogin)
+  },
+  {path:'sugestao', component: SugestaoComponent,
+  ...canActivate(enviarSemLogin)
+  },
+  { path:'direito', component: DireitoComponent,
+  ...canActivate(enviarSemLogin)
+  },
+  {
+    path:'psicologia', component: PsicologiaComponent,
+    ...canActivate(enviarSemLogin)
+  },
   {
     path:'sagas', component: SagasComponent,
-
+    ...canActivate(enviarSemLogin)
+  },
+  {
+    path:'artes', component: ArtesComponent,
+    ...canActivate(enviarSemLogin)
   }
-
 ];
 
 @NgModule({
