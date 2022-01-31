f33f6d5cd0bd236d2dec481d2cc0ebbd911d2ce1
Marina Farias <marinafarias.mk@gmail.com>
2022-01-31 20:55:29 -0300

    Alterado campos e inserido o conteúdo no endpoint.

--- src/app/criticas-literarias/modelos/criticasLiterarias.ts
+++ src/app/criticas-literarias/modelos/criticasLiterarias.ts
@@ -1,6 +1,10 @@
 export interface CriticasLiterarias {
   _idCriticaLit:string;
-  critico:string;
   titulo:string;
+  critico:string;
+  img: string;
   resumo:string;
+  link:[{ref:string}]
+  cols:number;
+  rows:number;
 }
